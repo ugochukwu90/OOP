d7aaffd0505daaf2170fae1bcb9638eca80102bb
ugochukwu90 <ugochiline@gmail.com>
2025-07-03 11:08:42 +0100

    Add files via upload

---
+++ Patient.java
@@ -0,0 +1,13 @@
+public  class Patient{
+      public String name;
+      private int mediaclID;
+      private String diagmosis;
+
+       public String getDiagnosis(String diag){
+         return diagmosis;
+       }
+
+       public void setDiagnosis(String diag){
+        diagmosis = diag;
+       
+}}
